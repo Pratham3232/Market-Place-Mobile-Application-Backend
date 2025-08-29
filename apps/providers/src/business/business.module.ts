7e6cbf38f66b47986d542db8293bc4bd5cdb6bff
Pratham3232 <prathamnigam32@gmail.com>
2025-08-29 18:28:47 +0530

    Dependency error fix

--- apps/providers/src/business/business.module.ts
+++ apps/providers/src/business/business.module.ts
@@ -1,8 +1,11 @@
+
 import { Module } from '@nestjs/common';
 import { BusinessService } from './business.service';
 import { BusinessController } from './business.controller';
+import { PrismaModule } from '../../../../libs/common/src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [BusinessController],
   providers: [BusinessService],
 })
--- apps/providers/src/providers.module.ts
+++ apps/providers/src/providers.module.ts
@@ -4,9 +4,10 @@ import { ProvidersService } from './providers.service';
 import { SoloModule } from './solo/solo.module';
 import { BusinessModule } from './business/business.module';
 import { LocationModule } from './location/location.module';
+import { PrismaModule } from '@app/common';
 
 @Module({
-  imports: [SoloModule, BusinessModule, LocationModule],
+  imports: [PrismaModule, SoloModule, BusinessModule, LocationModule],
   controllers: [ProvidersController],
   providers: [ProvidersService],
 })
