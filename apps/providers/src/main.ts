da14c5c7253d802938b270bb969d2c3655cee179
mehulsingh072001@gmail.com <mehulsingh072001@gmail.com>
2025-08-30 15:06:58 +0530

    docker file created

--- apps/providers/src/main.ts
+++ apps/providers/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { ProvidersModule } from './providers.module';
+import cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(ProvidersModule);
-  await app.listen(process.env.port ?? 3000);
+
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
+
+  const configService = app.get(ConfigService)
+  await app.startAllMicroservices();
+  await app.listen(configService.get("PROVIDERS_HTTP_PORT") || 3000);
 }
 bootstrap();
